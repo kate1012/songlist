b45b91e8b96a66f99d2ecc081a94e207d2bac032
Kaitlyn Sussman <ksussman12@icloud.com>
2015-02-12 09:07:25 -0500

    fixed stuff

--- SongLibrary.java
+++ SongLibrary.java
@@ -9,6 +9,8 @@ import javax.swing.event.ListSelectionListener;
 
 import java.awt.event.*;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class SongLibrary extends JFrame 
 {
@@ -21,6 +23,7 @@ public class SongLibrary extends JFrame
 	DefaultListModel listModel;
 	Song song1, song2, song3;
 	ArrayList<Song> storageList;
+	Boolean editClicked = false, listClicked = true;
 	
 	public SongLibrary()
 	{
@@ -41,14 +44,14 @@ public class SongLibrary extends JFrame
 		songListPanel = new JPanel();
 		songListPanel.setLayout(new BoxLayout(songListPanel, BoxLayout.Y_AXIS));
 		songListPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-		//songListPanel.setPreferredSize(new Dimension(200, 200));
-	
-		
+
 		//song details panel stuff
 		songDetailsPanel = new JPanel();
 		songDetailsPanel.setLayout(new BoxLayout(songDetailsPanel, BoxLayout.Y_AXIS));
 		songDetailsPanel.setBorder(BorderFactory.createLineBorder(Color.black));
 		songDetailsPanel.setPreferredSize(new Dimension(100, 100));
+		
+		
 		
 		//add song panel stuff
 		addSongPanel = new JPanel();
@@ -61,18 +64,18 @@ public class SongLibrary extends JFrame
 		song1 = new Song();
 		song2 = new Song();
 		song3 = new Song();
-		song1.setName("Name");
-		song1.setArtist("Artist");
-		song1.setAlbum("Album");
-		song1.setYear("Year");
-		song2.setName("Name");
-		song2.setArtist("Artist");
-		song2.setAlbum("Album");
-		song2.setYear("Year");
-		song3.setName("Name");
-		song3.setArtist("Artist");
-		song3.setAlbum("Album");
-		song3.setYear("Year");
+		song1.setName("Name1");
+		song1.setArtist("Artist1");
+		song1.setAlbum("Album1");
+		song1.setYear("Year1");
+		song2.setName("Name2");
+		song2.setArtist("Artist2");
+		song2.setAlbum("Album2");
+		song2.setYear("Year2");
+		song3.setName("Name3");
+		song3.setArtist("Artist3");
+		song3.setAlbum("Album3");
+		song3.setYear("Year3");
 		
 		//labels
 		songLabel = new JLabel("song name");
@@ -95,6 +98,19 @@ public class SongLibrary extends JFrame
 		artistToAddTb = new JTextField();
 		albumToAddTb = new JTextField();
 		yearToAddTb = new JTextField();
+		yearToAddTb.addKeyListener(new KeyAdapter() {
+		        @Override
+		        public void keyPressed(KeyEvent e) {
+
+		            int key = e.getKeyCode();
+
+		            /* Restrict input to only integers */
+		            if (key < 96 && key > 105) {}
+		            
+		            else
+		            	e.setKeyChar(e.getKeyChar());
+		        };
+		    });
 		
 		//buttons
 		add = new JButton("Add");
@@ -103,8 +119,10 @@ public class SongLibrary extends JFrame
 		//add.setBorder(BorderFactory.createLineBorder(Color.red));
 		delete = new JButton("Delete");
 		delete.addActionListener(lForButton);
+		delete.setEnabled(false);
 		edit = new JButton("Edit");
 		edit.addActionListener(lForButton);
+		edit.setEnabled(false);
 		submit = new JButton("Submit");
 		submit.addActionListener(lForButton);
 		
@@ -124,6 +142,12 @@ public class SongLibrary extends JFrame
 		addSongPanel.add(yearToAddTb);
 		addSongPanel.add(submit);
 		
+		//add labels to songDetailsPanel
+				songDetailsPanel.add(songLabel);
+				songDetailsPanel.add(artistLabel);
+				songDetailsPanel.add(albumLabel);
+				songDetailsPanel.add(yearLabel);
+		
 		createList();
 		//songListPanel.add(songlist);
 
@@ -133,7 +157,8 @@ public class SongLibrary extends JFrame
 		mainPanel.add(songListPanel, BorderLayout.WEST);
 		mainPanel.add(addSongPanel, BorderLayout.SOUTH);
 		
-		//addSongPanel.setVisible(false);
+		addSongPanel.setVisible(false);
+	
 		
 		//frame attributes 
 		this.add(mainPanel);
@@ -153,49 +178,21 @@ public class SongLibrary extends JFrame
 	
 	public void createList()
 	{	
+		//int selectedValue;
+		
+		//initialize new arraylist
 		storageList = new ArrayList<Song>();
 		storageList.add(song1);
 		storageList.add(song2);
 		storageList.add(song3);
 		
-		//song_list.add(listOfSongs);
-		
-		//songlist = new JList(listOfSongs);
-		
-		//Song[] songs = {song1, song2, song3};
-			
+		//Collections.sort(storageList.toString());
+
+		//initialize jlist 
 		listModel = new DefaultListModel();
-		//listModel.addElement("new element");
-		
 		songlist = new JList(listModel);
+		songlist.setSelectedIndex(0);
 		songlist.setLayoutOrientation(JList.VERTICAL);
-		songlist.addListSelectionListener(new ListSelectionListener(){
-			public void valueChanged(ListSelectionEvent ev)
-			{
-				
-				if(!ev.getValueIsAdjusting())
-				{
-					
-					JList source = (JList) ev.getSource();
-					int selectedValue = source.getSelectedIndex();
-					Song selectedSong = (Song) storageList.get(selectedValue);
-					songLabel.setText(selectedSong.getName());
-					System.out.println(selectedSong.getName().toString());
-					artistLabel.setText(selectedSong.getArtist());
-					albumLabel.setText(selectedSong.getAlbum());
-					yearLabel.setText(selectedSong.getYear());
-					yearLabel.setText(selectedSong.toString());
-					//albumLabel.setText(text);
-					
-					songDetailsPanel.add(songLabel);
-					songDetailsPanel.add(artistLabel);
-					songDetailsPanel.add(albumLabel);
-					songDetailsPanel.add(yearLabel);
-				}
-			}
-		});
-		
-		
 		songlist.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
 		songlist.setLayoutOrientation(JList.HORIZONTAL_WRAP);
 		songlist.setVisibleRowCount(-1);
@@ -203,73 +200,138 @@ public class SongLibrary extends JFrame
 		JScrollPane listScroller = new JScrollPane(songlist);
 		listScroller.setPreferredSize(new Dimension(250, 80));
 
+		//add arraylist elements to jlist
 		for(int i =0; i<storageList.size(); i++)
 		{
 			listModel.addElement(storageList.get(i).getName());
 		}
 		
+		songlist.addListSelectionListener(new ListSelectionListener(){
+			public void valueChanged(ListSelectionEvent ev)
+			{
+				
+				//if(!ev.getValueIsAdjusting())
+				//{
+					
+					
+					if(listClicked)
+					{
+						//enable delete and edit buttons
+						delete.setEnabled(true);
+						edit.setEnabled(true);
+			
+						int selectedValue = songlist.getSelectedIndex();
+						Song selectedSong = (Song) storageList.get(selectedValue);
+						songLabel.setText(selectedSong.getName());
+						artistLabel.setText(selectedSong.getArtist());
+						albumLabel.setText(selectedSong.getAlbum());
+						yearLabel.setText(selectedSong.getYear());
+						//yearLabel.setText(selectedSong.toString());
+		
+						
+					}
+					
+				}
+			//}
+		});
+		
+		
+		
+		
+		//add songlist to songlistpanel
 		songListPanel.add(songlist);	
-		//songListPanel.add(song_list);
+	}
+	
+	public void repopulateList()
+	{
+		listClicked = false;
+		
+		listModel.removeAllElements();
+
+		for(int i =0; i<storageList.size(); i++)
+		{
+			listModel.addElement(storageList.get(i).getName());
+		}
+		
+		listClicked = true;
+		
+		//songlist = new JList(listModel);
+	}
+	
+	public void removeElements(int index)
+	{
+		storageList.remove(index);
+		listModel.remove(index);
 	}
 	
 	class ListenForButton implements ActionListener
 	{
+		int selectedValue;
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			if(e.getSource()==add)
 			{
 				addSongPanel.setVisible(true);
-				System.out.println("add");
 			}
 			if(e.getSource()==delete)
 			{
 				int selectedIndex = songlist.getSelectedIndex();
 				Object selectedValue = songlist.getSelectedValue();
-				//listModel.removeElementAt(selectedIndex);
-				//listModel.remove(selectedIndex);
-				//listModel.remove(1);
 			}
 			if(e.getSource()==edit)
 			{
+				addSongPanel.setVisible(true);
+				editClicked = true;
+				
+				//retreive song from arraylist and populate text fields 
+				selectedValue = songlist.getSelectedIndex();
+				Song songToEdit = (Song) storageList.get(selectedValue);
+				songToAddTb.setText(songToEdit.getName());
+				artistToAddTb.setText(songToEdit.getArtist());
+				albumToAddTb.setText(songToEdit.getAlbum());
+				yearToAddTb.setText(songToEdit.getYear());
+				
+				//removeElements(selectedValue);
+				
+				
 				
 			}
 			if(e.getSource() == submit)
 			{
+				//create new song object and set its attributes 
+				Song newSong = new Song();
+				newSong.setName(songToAddTb.getText());
+				newSong.setArtist(artistToAddTb.getText());
+				newSong.setAlbum(albumToAddTb.getText());
+				newSong.setYear(yearToAddTb.getText());
 				
-				Song song = new Song();
-				song.setName(songToAddTb.getText());
-				song.setArtist(artistToAddTb.getText());
-				song.setAlbum(albumToAddTb.getText());
-				song.setYear(yearToAddTb.getText());
+				//add new song to listmodel and arraylist
 				
-				listModel.addElement(song.getName());
+				if(!(songToAddTb.getText().equals("") || artistToAddTb.getText().equals("")))
+				{
+					listModel.addElement(newSong.getName());
+					storageList.add(newSong);
+				}
+					
+				
+				
+				if(editClicked)
+				{
+					storageList.remove(selectedValue);
+					repopulateList();
+					//listModel.removeAllElements();
+					//storageList.remove(selectedValue);
+					//listModel.remove(selectedValue);
+				}
+				
+				
+				addSongPanel.setVisible(false);
+				
+				editClicked = false;
+				
+				//repopulateList();
 			}
 		}
 	}
 }
 
-/*class ListenForButton implements ActionListener
-{
-	SongLibrary sl;
-	
-	public ListenForButton(SongLibrary sl)
-	{
-		this.sl = sl;
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if(e.getSource()==sl.add)
-		{
-			sl.mainPanel.add(sl.addSongPanel, BorderLayout.SOUTH);
-		}
-		if(e.getSource()==sl.delete)
-		{
-			
-		}
-		if(e.getSource()==sl.edit)
-		{
-			
-		}
-	}
-}*/
